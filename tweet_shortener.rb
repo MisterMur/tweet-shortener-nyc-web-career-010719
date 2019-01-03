53c4d55140e688e3e33fa15b7b5b05428d35c073
Brian Murillo <brian.j.murillo@gmail.com>
2019-01-03 19:28:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def dictionary 
+  
+  {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweetarr = tweet.split(' ')
+  tweetarr.each do |word|
+    dictionary.each do |key,value|
+      if word == key
+        word = value
+      end
+    end
+  end
+  tweetstr = tweetarr.join(' ')
+  
+  return tweetstr
+end
