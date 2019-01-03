43d6a4d388c35c13bd09e96efd840d3d6b922c3d
Brian Murillo <brian.j.murillo@gmail.com>
2019-01-03 19:38:43 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -28,7 +28,7 @@ end
 def word_substituter(tweet)
   tweetarr = tweet.split(' ')
   tweetarr.map do |word|
-    dictionary.each do |k,v|
+    dictionary.map do |k,v|
       if word == k
         # puts "in if"
         word = v.to_s
