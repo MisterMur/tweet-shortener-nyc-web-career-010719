aa22cd5201d3ab48cb2586c88d1830a19f206175
Brian Murillo <brian.j.murillo@gmail.com>
2019-01-03 19:50:04 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,16 +3,16 @@
 def dictionary 
   
   {
-    "hello" => "hi",
-    "to" => "2",
-    "two" => "2",
-    "too" => "2",
-    "for" => "4",
-    "four" => "4",
-    "be" => "b",
-    "you" => "u",
-    "at" => "@",
-    "and" => "&"
+    "Hello" => "hi",
+    "To" => "2",
+    "Two" => "2",
+    "Too" => "2",
+    "For" => "4",
+    "Four" => "4",
+    "Be" => "b",
+    "You" => "u",
+    "At" => "@",
+    "And" => "&"
   }
 end
 
@@ -30,7 +30,7 @@ def word_substituter(tweet)
   tweetarr = tweet.split(' ')
   newtweet = []
   tweetarr.each do |word|
-    if dictionary.key?(word) 
+    if dictionary.key?(word.capitalize) 
         newtweet << dictionary[word].to_s
         
       else 
